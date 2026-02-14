fff3a1ad8384b69ddc0236d8ceeeefb1c6d335d2
ZiadMostafa01 <ziadmostafa9866@gmail.com>
2026-02-15 01:05:18 +0200

    Add React project

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,55 @@
+// import { Routes, Route } from "react-router-dom";
+// import AOS from "aos";
+// import "aos/dist/aos.css";
+
+// function App() {
+//   useEffect(() => {
+//     AOS.init({
+//       duration: 800,
+//       easing: "ease-out-cubic",
+//       once: true, // الأنيميشن يشتغل مرة واحدة
+//       offset: 80,
+//     });
+//   }, []);
+//   return (
+//     <>
+//       <ScrollToTop />
+
+//       <Routes>
+//         {/* Main Layout */}
+//         <Route path="/" element={<MainLayout />}>
+//           <Route index element={<Home />} />
+//           <Route path="months-courses" element={<MonthsCourses />} />
+//           <Route path="packages" element={<PackageCourses />} />
+//           <Route path="pre-media" element={<PreMedia />} />
+
+//           <Route path="login" element={<Login />} />
+//           <Route path="register" element={<Register />} />
+//         </Route>
+
+//         {/* Profile Layout */}
+//         <Route path="/profile" element={<ProfileLayout />}>
+//           <Route index element={<ProfileInfo />} />
+//           <Route path="subscriptions" element={<Subscriptions />} />
+//           <Route path="wallet" element={<Wallet />} />
+//           <Route path="invoices" element={<Invoices />} />
+//           <Route path="exams" element={<Exams />} />
+//         </Route>
+
+//         {/* Media Layout */}
+//         <Route path="/media" element={<MediaPreview />}>
+//           <Route index element={<VideoLesson />} />
+//           <Route path="homework" element={<HomeworkVideo />} />
+//           <Route path="lesson-exam" element={<LessonExam />} />
+//           <Route path="lesson-file" element={<LessonFile />} />
+//         </Route>
+//       </Routes>
+//     </>
+//   );
+// }
+
+// export default App;
 import { Routes, Route } from "react-router-dom";
-import ScrollToTop from "./components/layout/ScrollToTop";
-
-import MainLayout from "./layouts/MainLayout";
-import ProfileLayout from "./layouts/ProfileLayout";
-import MediaPreview from "./layouts/MediaPreview";
-
-import Home from "./pages/Home";
-import MonthsCourses from "./pages/courses/MonthsCourses";
-import PackageCourses from "./pages/courses/PackageCourses";
-import PreMedia from "./pages/courses/PreMedia";
-
-import Login from "./pages/auth/Login";
-import Register from "./pages/auth/Register";
-
-import ProfileInfo from "./pages/profile/ProfileInfo";
-import Subscriptions from "./pages/profile/Subscriptions";
-import Wallet from "./pages/profile/Wallet";
-import Invoices from "./pages/profile/Invoices";
-import Exams from "./pages/profile/Exams";
-
-import VideoLesson from "./pages/media/VideoLesson";
-import HomeworkVideo from "./pages/media/HomeworkVideo";
-import LessonExam from "./pages/media/LessonExam";
-import LessonFile from "./pages/media/LessonFile";
-
-import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -39,37 +64,7 @@ function App() {
   }, []);
   return (
     <>
-      <ScrollToTop />
-
-      <Routes>
-        {/* Main Layout */}
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="months-courses" element={<MonthsCourses />} />
-          <Route path="packages" element={<PackageCourses />} />
-          <Route path="pre-media" element={<PreMedia />} />
-
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-        </Route>
-
-        {/* Profile Layout */}
-        <Route path="/profile" element={<ProfileLayout />}>
-          <Route index element={<ProfileInfo />} />
-          <Route path="subscriptions" element={<Subscriptions />} />
-          <Route path="wallet" element={<Wallet />} />
-          <Route path="invoices" element={<Invoices />} />
-          <Route path="exams" element={<Exams />} />
-        </Route>
-
-        {/* Media Layout */}
-        <Route path="/media" element={<MediaPreview />}>
-          <Route index element={<VideoLesson />} />
-          <Route path="homework" element={<HomeworkVideo />} />
-          <Route path="lesson-exam" element={<LessonExam />} />
-          <Route path="lesson-file" element={<LessonFile />} />
-        </Route>
-      </Routes>
+      <home />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-import { AuthProvider } from "./context/AuthContext";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <BrowserRouter basename="/educational-platform/">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
-    </AuthProvider>
   </React.StrictMode>,
 );
